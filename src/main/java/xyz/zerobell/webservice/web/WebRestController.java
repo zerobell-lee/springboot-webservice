46be590ad69c335dce534237a14a62441c7fdb0a
zerobell-lee <bnb3456@gmail.com>
2019-04-24 14:31:15 +0900

    first commit

---
+++ src/main/java/xyz/zerobell/webservice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package xyz.zerobell.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
